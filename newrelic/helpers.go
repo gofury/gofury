f7176693b242d5f3cd2c3638286145699db243f8
Kale Worsley <kale@worsley.co.nz>
2016-11-10 10:56:00 +1300

    Use a more generic application name.

--- newrelic/helpers.go
+++ newrelic/helpers.go
@@ -4,7 +4,7 @@ import "github.com/newrelic/go-agent"
 
 // NullNewRelic returns a disabled New Relic appliaction.
 func NullNewRelic() newrelic.Application {
-	config := newrelic.NewConfig("smsprocessor", "")
+	config := newrelic.NewConfig("application", "")
 	config.Enabled = false
 	app, _ := newrelic.NewApplication(config)
 
